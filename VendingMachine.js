5d1e40e95af878f6fded783f1887324bbf6392b4
Cameron Wise <wisecameron1@gmail.com>
2024-10-07 05:09:48 -0400

    vending machine class
    added some defined errors, cleaned up some hard coded logic

---
+++ VendingMachine.js
@@ -0,0 +1,73 @@
+class VendingMachine {
+    constructor(inventory, drinkPrice, acceptedCoins){
+        this.inventory = inventory;
+        this.drinkPrice = drinkPrice;
+        this.acceptedCoins = acceptedCoins;
+        this.quantityDispensed = 0;
+        this.errors = {
+            INVALID_COIN: { id: 1001, message: 'Please insert a single US quarter', status: 500 },
+            INSUFFICIENT_FUNDS: { id: 1002, message: 'Insufficient funds',  status: 403 },
+            OUT_OF_STOCK: { id: 1003, message: 'Out of stock',  status: 404  },
+            INVALID_ID: { id: 1004, message: `Please enter a valid ID[1 - ${this.inventory.length}]`, status: 500  },
+        };
+    }
+
+    getItemStock(id) {
+        // if ID is not a number or is not a defined value in the array throw an error
+        if(typeof id !== 'number' || typeof this.inventory[id] === 'undefined'){
+            throw this.errors.INVALID_ID;
+        }
+        // adjust id to zero indexed array
+        id -= 1;
+
+        
+        console.log(this.inventory[id])
+        return this.inventory[id];
+    }
+
+    insertCoin(coin){
+        
+        //if coin is not defined or exactly 1 return an error
+        if(typeof coin === 'undefined' || coin !== 1){
+            throw this.errors.INVALID_COIN;
+        }
+         
+        this.acceptedCoins += 1;
+    }
+
+    purchaseItem(id, callback) {
+
+        // if ID is not a number or is not a defined value in the array throw an error
+        if(typeof id !== 'number' || typeof this.inventory[id] === 'undefined'){
+            throw this.errors.INVALID_ID;
+        }
+
+        // adjust id to zero indexed array
+        id -= 1;
+
+        if(this.acceptedCoins < this.drinkPrice){
+            // current accepted coins greater than cost of drink, throw insufficent fund error
+            throw this.errors.INSUFFICIENT_FUNDS;
+        } else if (this.inventory[id] === 0){
+            // selected item out of inventory
+            throw this.errors.OUT_OF_STOCK;
+        } else if(this.inventory[id] > 0) {
+            
+            // remove cost of drinks from accepted coins, remove purchased item from stock, and increment dispensed counter
+            this.acceptedCoins -= this.drinkPrice;
+            this.inventory[id] -= 1;
+            this.quantityDispensed += 1;
+
+            callback();
+            // We want to display a success response with current accepted coins before returning the coins
+            // Transaction complete, coins were returned, dispensed count reset
+            this.acceptedCoins = 0;
+            this.quantityDispensed = 0;
+            return;
+        } 
+    }
+    returnCoins(){
+        this.acceptedCoins = 0;
+    }
+    
+} module.exports = VendingMachine;
--- index.js
+++ index.js
@@ -2,100 +2,83 @@ const express = require('express');
 const app = express();
 app.use(express.json()); // To parse JSON request bodies
 
+const VendingMachine = require('./VendingMachine');
+
 const port = 3000;
+// Assuming IDs are 1-3
+const vending = new VendingMachine([5,5,5], 2, 0);
 
-let acceptedCoins = 0;
-const inventory = [5, 5, 5]
-const DRINK_PRICE = 2;
 
-app.get('/', (req, res) => {
-    res.send('<h1> vending time </h1>');
-});
-
-app.put('/', (req, res, next) => {
-
-    
+app.put('/', (req, res) => {
     const {coin} = req.body;
-
-    if(typeof coin === 'undefined' || coin !== 1){
-        return res.status(403).send('Please insert a single US quarter')
+    try{
+        vending.insertCoin(coin)
+        return res.set('X-Coins', vending.acceptedCoins)
+            .status(204)
+            .send();
+    } catch (error) {
+        if(error.id){
+            return res.status(error.status).send(error.message)
+        }
     }
-
-    acceptedCoins += coin;
-
-    res.set('X-Coins', acceptedCoins)
-        .status(204)
-        .send();
-
 })
 
 app.get('/inventory', (req, res) => {
-    res.status(200)
-        .send(inventory)
+    return res.status(200)
+        .send(vending.inventory);
 })
 
 app.delete('/', (req, res) => {
-    res.status(204)
-        .set('X-Coins', acceptedCoins)
+
+    // get coin count before returning to user
+    const remainingCoins = vending.acceptedCoins
+    vending.returnCoins();
+
+    return res.status(204)
+        .set('X-Coins', remainingCoins)
         .send();
 
-    acceptedCoins = 0;
 })
 
-// Assuming IDs are 1-3
-app.get('/inventory/:id', (req, res, next) => {
+app.get('/inventory/:id', (req, res) => {
     // basic id param validation
     let id = req.params.id;
-
     // If a non integer string is passed id will return null
     id = parseInt(id);
-    
-    if(id > 3 || id < 1 || typeof id !== 'number' ){
-        return res.status(403).send('Please enter a valid ID[1-3]')
+    try{
+       return res.status(200)
+            .send(`${ vending.getItemStock(id)}`);
     }
-
-    if(inventory[id + 1]) {
-        res.status(200).send(`${inventory[id + 1]}`);
-    } else {
-        throw error;
+    catch (error) {
+        if(error.id){
+            return res.status(error.status).send(error.message)
+        }
     }
-
 })
 
-app.put('/inventory/:id', (req, res, next) => {
-    try{
-        let {id} = req.params;
-
-        id = parseInt(id);
-
-        if(id > 3 || id < 1 || typeof id !== 'number' ){
-            throw new Error('Please enter a valid ID[1-3]')
-        }
-        //Coins response header is returned in all responses
-        res.set('X-Coins', acceptedCoins)
-        if(acceptedCoins < DRINK_PRICE){
-            return res.status(403)
-                .send()
-        } else if (inventory[id] === 0){
-            return res.status(404)
-                .send()
-        } if(inventory[id] > 0) {
-            acceptedCoins -= DRINK_PRICE;
-            inventory[id] -= 1;
-            res.set('X-Inventory-Remaining', inventory[id + 1])
+app.put('/inventory/:id', (req, res) => {
+    
+    // basic id param validation
+    let id = req.params.id;
+    // If a non integer string is passed id will return null
+    id = parseInt(id);
+    try {
+        // We want to return the quantity, and coins returned before resetting the values 
+        const callback = () => {
+            res.set('X-Inventory-Remaining', vending.getItemStock(id))
+                .set('X-Coins', vending.acceptedCoins)
                 .status(200)
-                .send(`quantity: 1`);    
-            
-            //Transaction complete, coins were returned
-            acceptedCoins = 0;
-            return;
+                .send(`quantity: ${vending.quantityDispensed}`);    
+        }
+        return vending.purchaseItem(id, callback)
+    } catch (error) {
+        if(error.id){
+            return res.set('X-Coins', vending.acceptedCoins)
+                .status(error.status)
+                .send(error.message)
         } 
     }
-    catch (error){
-        next(error)
-    }
 })
-
 
 app.listen(port, () => {
     console.log(`Server running at ${port}`)
